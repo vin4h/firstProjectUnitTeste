44a037ff55b2b5533a48c8708344b29c0fc0790f
Vinicius Freitas <viniciu.freitas@gmail.com>
2021-09-29 22:34:46 -0400

    Added route post using addCompany '

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -23,6 +23,18 @@ const addCompany = ({ socialReason, cnpj, address }: CompanyConstructor) => {
 
 companyRouter.get('/company', (request, response) => {
     response.json(getCompanies());
+});
+
+companyRouter.post('/company', (request, response) => {
+    const { socialReason, cnpj, address } = request.body;
+
+    const newCompany = addCompany({
+        socialReason,
+        cnpj,
+        address
+    });
+
+    return response.status(201).json(newCompany);
 })
 
 export default { companyRouter, getCompanies, addCompany };
